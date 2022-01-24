359fa27b9d5986f3b0c5a35a8b57a0ca3bd8eaca
Schmidt-Dumont Georg (BCI/ESW17) <sgh4fe@bosch.com>
2022-01-24 14:52:58 +0100

    Add missing copyright headers

--- core/sds-aspect-meta-model-version-migrator/src/main/java/io/openmanufacturing/sds/aspectmodel/versionupdate/migrator/SdsAnonymousEnumerationMigrator.java
+++ core/sds-aspect-meta-model-version-migrator/src/main/java/io/openmanufacturing/sds/aspectmodel/versionupdate/migrator/SdsAnonymousEnumerationMigrator.java
@@ -1,7 +1,19 @@
+/*
+ * Copyright (c) 2022 Robert Bosch Manufacturing Solutions GmbH
+ *
+ * See the AUTHORS file(s) distributed with this work for additional
+ * information regarding authorship.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 package io.openmanufacturing.sds.aspectmodel.versionupdate.migrator;
 
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.apache.jena.rdf.model.Model;
