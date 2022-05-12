2a69b4774a7ad47273ced5bbef8323528c728ece
Johannes Kristan <johannes.kristan@bosch.io>
2022-05-12 22:58:41 +0200

    Reformatted with code formatter according to google coding guidelines
    Signed-off-by: Johannes Kristan <johannes.kristan@bosch.io>

--- tools/bamm-cli/src/main/java/io/openmanufacturing/sds/aspect/to/AspectToAasCommand.java
+++ tools/bamm-cli/src/main/java/io/openmanufacturing/sds/aspect/to/AspectToAasCommand.java
@@ -18,49 +18,60 @@ import java.io.IOException;
 import io.openmanufacturing.sds.AbstractCommand;
 import io.openmanufacturing.sds.aspect.AspectToCommand;
 import io.openmanufacturing.sds.aspectmodel.aas.AspectModelAASGenerator;
-import io.openmanufacturing.sds.aspectmodel.generator.json.AspectModelJsonPayloadGenerator;
 import io.openmanufacturing.sds.exeption.CommandException;
 import io.openmanufacturing.sds.metamodel.Aspect;
 import io.openmanufacturing.sds.metamodel.loader.AspectModelLoader;
 import picocli.CommandLine;
 
-@CommandLine.Command( name = AspectToAasCommand.COMMAND_NAME,
-      description = "Generate Asset Administration Shell (AAS) submodel template for an Aspect Model",
-      descriptionHeading = "%n@|bold Description|@:%n%n",
-      parameterListHeading = "%n@|bold Parameters|@:%n",
-      optionListHeading = "%n@|bold Options|@:%n",
-      mixinStandardHelpOptions = true
-)
+@CommandLine.Command(
+    name = AspectToAasCommand.COMMAND_NAME,
+    description = "Generate Asset Administration Shell (AAS) submodel template for an Aspect Model",
+    descriptionHeading = "%n@|bold Description|@:%n%n",
+    parameterListHeading = "%n@|bold Parameters|@:%n",
+    optionListHeading = "%n@|bold Options|@:%n",
+    mixinStandardHelpOptions = true)
 public class AspectToAasCommand extends AbstractCommand {
 
-   public static final String COMMAND_NAME = "aas";
-   public static final String AASX = "aasx";
-   public static final String XML = "xml";
+  public static final String COMMAND_NAME = "aas";
+  public static final String AASX = "aasx";
+  public static final String XML = "xml";
 
-   @CommandLine.Option( names = { "--output", "-o" }, description = "Output file path" )
-   private String outputFilePath = "-";
+  @CommandLine.Option(
+      names = {"--output", "-o"},
+      description = "Output file path")
+  private String outputFilePath = "-";
 
-   @CommandLine.Option( names = { "--format", "-f" }, description = "The file format the AAS is to be generated. Valid options are \"" + AASX
-         + "\" and \"" + XML + "\". Default is \"" + XML + "\"." )
-   private String format = XML;
+  @CommandLine.Option(
+      names = {"--format", "-f"},
+      description =
+          "The file format the AAS is to be generated. Valid options are \""
+              + AASX
+              + "\" and \""
+              + XML
+              + "\". Default is \""
+              + XML
+              + "\".")
+  private String format = XML;
 
-   @CommandLine.ParentCommand
-   private AspectToCommand parentCommand;
+  @CommandLine.ParentCommand private AspectToCommand parentCommand;
 
-   @Override
-   public void run() {
-      final AspectModelAASGenerator generator = new AspectModelAASGenerator();
-      final Aspect aspect = AspectModelLoader.fromVersionedModelUnchecked(loadModelOrFail(parentCommand.parentCommand.getInput()));
-      try {
-         // we intentionally override the name of the generated artifact here to the name explicitly desired by the user (outputFilePath),
-         // as opposed to what the model thinks it should be called (name)
-         if( format.equals(AASX) ){
-            generator.generateAASXFile(aspect, name -> getStreamForFile( outputFilePath ));
-         } else {
-            generator.generateAasXmlFile(aspect, name -> getStreamForFile( outputFilePath ));
-         }
-      } catch ( final IOException e ) {
-         throw new CommandException( e );
+  @Override
+  public void run() {
+    final AspectModelAASGenerator generator = new AspectModelAASGenerator();
+    final Aspect aspect =
+        AspectModelLoader.fromVersionedModelUnchecked(
+            loadModelOrFail(parentCommand.parentCommand.getInput()));
+    try {
+      // we intentionally override the name of the generated artifact here to the name explicitly
+      // desired by the user (outputFilePath),
+      // as opposed to what the model thinks it should be called (name)
+      if (format.equals(AASX)) {
+        generator.generateAASXFile(aspect, name -> getStreamForFile(outputFilePath));
+      } else {
+        generator.generateAasXmlFile(aspect, name -> getStreamForFile(outputFilePath));
       }
-   }
+    } catch (final IOException e) {
+      throw new CommandException(e);
+    }
+  }
 }
