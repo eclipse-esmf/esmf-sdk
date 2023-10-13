d4481fa07efecfa51f6fefd26099c8559ccb9dc2
Textor Andreas (BCI/ESW17) <Andreas.Textor@de.bosch.com>
2023-10-13 16:03:12 +0200

    Fix visitor for traversing aspect models and LanguageCollector

--- core/esmf-aspect-meta-model-java/src/main/java/org/eclipse/esmf/metamodel/visitor/LanguageCollectorModelVisitor.java
+++ core/esmf-aspect-meta-model-java/src/main/java/org/eclipse/esmf/metamodel/visitor/LanguageCollectorModelVisitor.java
@@ -20,26 +20,40 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.eclipse.esmf.characteristic.Collection;
+import org.eclipse.esmf.characteristic.Quantifiable;
 import org.eclipse.esmf.characteristic.Trait;
 import org.eclipse.esmf.metamodel.Aspect;
+import org.eclipse.esmf.metamodel.Characteristic;
 import org.eclipse.esmf.metamodel.Entity;
 import org.eclipse.esmf.metamodel.ModelElement;
 import org.eclipse.esmf.metamodel.NamedElement;
 import org.eclipse.esmf.metamodel.Operation;
 import org.eclipse.esmf.metamodel.Property;
-import org.eclipse.esmf.metamodel.QuantityKind;
-import org.eclipse.esmf.metamodel.Unit;
 import org.eclipse.esmf.metamodel.datatypes.LangString;
 
 import com.google.common.collect.Sets;
 
 /**
  * Aspect Model Visitor that retrieves all used Locales in an Aspect Model
+ *
+ * @deprecated Use org.eclipse.esmf.aspectmodel.generator.LanguageCollector instead
  */
+@Deprecated( forRemoval = true )
 public class LanguageCollectorModelVisitor implements AspectVisitor<Set<Locale>, Set<Locale>> {
    @Override
    public Set<Locale> visitBase( final ModelElement modelElement, final Set<Locale> context ) {
+      if ( modelElement instanceof final NamedElement described ) {
+         return visitIsDescribed( described, context );
+      }
       return context;
+   }
+
+   @Override
+   public Set<Locale> visitCharacteristic( final Characteristic characteristic, final Set<Locale> context ) {
+      return Stream.of(
+                  Stream.of( visitIsDescribed( characteristic, context ) ),
+                  characteristic.getDataType().stream().map( type -> type.accept( this, Collections.emptySet() ) ) )
+            .reduce( Stream.empty(), Stream::concat ).reduce( context, Sets::union );
    }
 
    public Set<Locale> visitIsDescribed( final NamedElement element, final Set<Locale> context ) {
@@ -54,7 +68,8 @@ public class LanguageCollectorModelVisitor implements AspectVisitor<Set<Locale>,
       return Stream.of(
                   Stream.of( visitIsDescribed( aspect, nestedContext ) ),
                   aspect.getProperties().stream().map( property -> property.accept( this, Collections.emptySet() ) ),
-                  aspect.getOperations().stream().map( operation -> operation.accept( this, Collections.emptySet() ) ) )
+                  aspect.getOperations().stream().map( operation -> operation.accept( this, Collections.emptySet() ) ),
+                  aspect.getEvents().stream().map( event -> event.accept( this, Collections.emptySet() ) ) )
             .reduce( Stream.empty(), Stream::concat ).reduce( nestedContext, Sets::union );
    }
 
@@ -62,7 +77,8 @@ public class LanguageCollectorModelVisitor implements AspectVisitor<Set<Locale>,
    public Set<Locale> visitProperty( final Property property, final Set<Locale> context ) {
       return Stream.of(
                   Stream.of( visitIsDescribed( property, context ) ),
-                  property.getCharacteristic().stream().map( characteristic -> characteristic.accept( this, Collections.emptySet() ) ) )
+                  property.getCharacteristic().stream().map( characteristic -> characteristic.accept( this, Collections.emptySet() ) ),
+                  property.getExtends().stream().map( superProperty -> superProperty.accept( this, Collections.emptySet() ) ) )
             .reduce( Stream.empty(), Stream::concat ).reduce( context, Sets::union );
    }
 
@@ -89,23 +105,27 @@ public class LanguageCollectorModelVisitor implements AspectVisitor<Set<Locale>,
    public Set<Locale> visitEntity( final Entity entity, final Set<Locale> context ) {
       return Stream.of(
                   Stream.of( visitIsDescribed( entity, context ) ),
-                  entity.getProperties().stream().map( property -> property.accept( this, Collections.emptySet() ) ) )
+                  entity.getProperties().stream().map( property -> property.accept( this, Collections.emptySet() ) ),
+                  entity.getExtends().stream().map( superEntity -> superEntity.accept( this, Collections.emptySet() ) ) )
             .reduce( Stream.empty(), Stream::concat ).reduce( context, Sets::union );
    }
 
    @Override
-   public Set<Locale> visitUnit( final Unit unit, final Set<Locale> context ) {
-      return Collections.emptySet();
-   }
-
-   @Override
-   public Set<Locale> visitQuantityKind( final QuantityKind quantityKind, final Set<Locale> context ) {
-      return Collections.emptySet();
-   }
-
-   @Override
    public Set<Locale> visitCollection( final Collection collection, final Set<Locale> context ) {
-      return collection.getElementCharacteristic().isPresent() ? collection.getElementCharacteristic().get().accept( this, Collections.emptySet() ) :
-            collection.getDataType().get().accept( this, Collections.emptySet() );
+      return Stream.of(
+                  Stream.of( visitIsDescribed( collection, context ) ),
+                  collection.getElementCharacteristic().stream().map( characteristic -> characteristic.accept( this,
+                        Collections.emptySet() ) ),
+                  collection.getDataType().stream().map( type -> type.accept( this, Collections.emptySet() ) ) )
+            .reduce( Stream.empty(), Stream::concat ).reduce( context, Sets::union );
+   }
+
+   @Override
+   public Set<Locale> visitQuantifiable( final Quantifiable quantifiable, final Set<Locale> context ) {
+      return Stream.of(
+                  Stream.of( visitIsDescribed( quantifiable, context ) ),
+                  quantifiable.getUnit().stream().map( unit -> unit.accept( this, Collections.emptySet() ) ),
+                  quantifiable.getDataType().stream().map( type -> type.accept( this, Collections.emptySet() ) ) )
+            .reduce( Stream.empty(), Stream::concat ).reduce( context, Sets::union );
    }
 }
