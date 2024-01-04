b0ea9d12801cbc3c8d0bbe75be6cf3a913093bce
Textor Andreas (BCI/ESW17) <Andreas.Textor@de.bosch.com>
2024-01-04 07:38:16 +0100

    Add invalid target format test for Maven plugin AAS generation

--- tools/esmf-aspect-model-maven-plugin/src/test/java/org/eclipse/esmf/aspectmodel/GenerateAasTest.java
+++ tools/esmf-aspect-model-maven-plugin/src/test/java/org/eclipse/esmf/aspectmodel/GenerateAasTest.java
@@ -18,6 +18,7 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 import java.io.File;
 
 import org.apache.maven.plugin.Mojo;
+import org.apache.maven.plugin.MojoExecutionException;
 import org.junit.Test;
 
 public class GenerateAasTest extends AspectModelMojoTest {
@@ -29,5 +30,15 @@ public class GenerateAasTest extends AspectModelMojoTest {
 
       assertGeneratedFileExists( "Aspect.xml" );
       deleteGeneratedFile( "Aspect.xml" );
+   }
+
+   @Test
+   public void testGenerateAasInvalidTargetFormat() throws Exception {
+      final File testPom = getTestFile( "src/test/resources/generate-aas-pom-invalid-target-format.xml" );
+      final Mojo generateAas = lookupMojo( "generateAas", testPom );
+
+      assertThatCode( generateAas::execute )
+            .isInstanceOf( MojoExecutionException.class )
+            .hasMessage( "Invalid target format: html. Valid formats are aasx, xml, json." );
    }
 }
