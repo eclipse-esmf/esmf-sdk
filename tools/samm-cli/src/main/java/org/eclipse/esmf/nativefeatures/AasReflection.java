a55b03195a82ed0b18d7256bb5d21950076798c3
Textor Andreas (BCI/ESW17) <Andreas.Textor@de.bosch.com>
2024-01-24 16:28:45 +0100

    Mark reflection configuration feature as hosted only

--- tools/samm-cli/src/main/java/org/eclipse/esmf/nativefeatures/AasReflection.java
+++ tools/samm-cli/src/main/java/org/eclipse/esmf/nativefeatures/AasReflection.java
@@ -14,11 +14,14 @@
 package org.eclipse.esmf.nativefeatures;
 
 import org.eclipse.digitaltwin.aas4j.v3.dataformat.core.util.ReflectionHelper;
+import org.graalvm.nativeimage.Platform;
+import org.graalvm.nativeimage.Platforms;
 import org.graalvm.nativeimage.hosted.Feature;
 
 /**
  * Registers all classes in the AAS model and default implementation packages for reflection in native image builds.
  */
+@Platforms( Platform.HOSTED_ONLY.class )
 public class AasReflection extends RegisterReflection implements Feature {
    @Override
    public void beforeAnalysis( final BeforeAnalysisAccess access ) {
