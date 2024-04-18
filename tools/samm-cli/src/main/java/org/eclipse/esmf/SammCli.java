34a2dc012641a2b4a72816e06646411e3f8d5316
Textor Andreas (BCI/ESW17) <Andreas.Textor@de.bosch.com>
2024-04-18 14:48:53 +0200

    Remove unreliable System.console check

--- tools/samm-cli/src/main/java/org/eclipse/esmf/SammCli.java
+++ tools/samm-cli/src/main/java/org/eclipse/esmf/SammCli.java
@@ -89,9 +89,9 @@ public class SammCli extends AbstractCommand {
    }
 
    public static void main( final String[] argv ) {
-      // Check if the .exe was started from the desktop/explorer.
+      // Check if the .exe was started on Windows without arguments: Most likely opened from Explorer or Desktop.
       // If yes, open a command prompt to continue working instead.
-      if ( new IsWindows().getAsBoolean() && System.console() == null && argv.length == 0 ) {
+      if ( new IsWindows().getAsBoolean() && argv.length == 0 ) {
          ProcessHandle.current().info().command().ifPresent( executable -> {
             // Only spawn terminals for native executable
             if ( !executable.endsWith( "java.exe" ) ) {
