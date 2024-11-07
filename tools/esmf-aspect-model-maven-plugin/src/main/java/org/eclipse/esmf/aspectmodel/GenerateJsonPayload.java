30dc4de4656226a386b28079ca97ada800d71553
Textor Andreas (BCI/ESW17) <Andreas.Textor@de.bosch.com>
2024-11-07 16:10:28 +0100

    Enable configuring @type attribute for JSON payload in Maven plugin

--- tools/esmf-aspect-model-maven-plugin/src/main/java/org/eclipse/esmf/aspectmodel/GenerateJsonPayload.java
+++ tools/esmf-aspect-model-maven-plugin/src/main/java/org/eclipse/esmf/aspectmodel/GenerateJsonPayload.java
@@ -16,12 +16,15 @@ package org.eclipse.esmf.aspectmodel;
 import java.util.Set;
 
 import org.eclipse.esmf.aspectmodel.generator.json.AspectModelJsonPayloadGenerator;
+import org.eclipse.esmf.aspectmodel.generator.json.JsonPayloadGenerationConfig;
+import org.eclipse.esmf.aspectmodel.generator.json.JsonPayloadGenerationConfigBuilder;
 import org.eclipse.esmf.metamodel.Aspect;
 
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -29,13 +32,19 @@ import org.slf4j.LoggerFactory;
 public class GenerateJsonPayload extends AspectModelMojo {
    private static final Logger LOG = LoggerFactory.getLogger( GenerateJsonPayload.class );
 
+   @Parameter( defaultValue = "false" )
+   private boolean addTypeAttribute;
+
    @Override
    public void executeGeneration() throws MojoExecutionException, MojoFailureException {
       validateParameters();
 
       final Set<Aspect> aspects = loadAspects();
       for ( final Aspect context : aspects ) {
-         final AspectModelJsonPayloadGenerator generator = new AspectModelJsonPayloadGenerator( context );
+         final JsonPayloadGenerationConfig config = JsonPayloadGenerationConfigBuilder.builder()
+               .addTypeAttributeForEntityInheritance( addTypeAttribute )
+               .build();
+         final AspectModelJsonPayloadGenerator generator = new AspectModelJsonPayloadGenerator( context, config );
          generator.generate( name -> getOutputStreamForFile( name + ".json", outputDirectory ) );
       }
       LOG.info( "Successfully generated example JSON payloads for Aspect Models." );
