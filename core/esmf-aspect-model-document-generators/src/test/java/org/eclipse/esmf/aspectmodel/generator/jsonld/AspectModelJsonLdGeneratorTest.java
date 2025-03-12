8b3b7ba571cb9b38d0f42ee03bd7bfd7e8f4da0e
Textor Andreas (BCI/ESW17) <Andreas.Textor@de.bosch.com>
2025-03-12 07:13:45 +0100

    Fix failing test

--- core/esmf-aspect-model-document-generators/src/test/java/org/eclipse/esmf/aspectmodel/generator/jsonld/AspectModelJsonLdGeneratorTest.java
+++ core/esmf-aspect-model-document-generators/src/test/java/org/eclipse/esmf/aspectmodel/generator/jsonld/AspectModelJsonLdGeneratorTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.eclipse.esmf.aspectmodel.urn.AspectModelUrn;
 import org.eclipse.esmf.metamodel.Aspect;
+import org.eclipse.esmf.samm.KnownVersion;
 import org.eclipse.esmf.test.TestAspect;
 import org.eclipse.esmf.test.TestResources;
 
@@ -22,8 +23,10 @@ class AspectModelJsonLdGeneratorTest {
    private void assertJsonLdMeta( final String generatedJsonLd, final AspectModelUrn aspectModelUrn ) {
       assertThat( generatedJsonLd ).contains( "\"@graph\" : [" );
       assertThat( generatedJsonLd ).contains( "\"xsd\" : \"http://www.w3.org/2001/XMLSchema#\"," );
-      assertThat( generatedJsonLd ).contains( "\"samm-c\" : \"urn:samm:org.eclipse.esmf.samm:characteristic:2.1.0#\"" );
-      assertThat( generatedJsonLd ).contains( "\"samm\" : \"urn:samm:org.eclipse.esmf.samm:meta-model:2.1.0#\"" );
+      assertThat( generatedJsonLd ).contains(
+            "\"samm-c\" : \"urn:samm:org.eclipse.esmf.samm:characteristic:" + KnownVersion.getLatest().toVersionString() + "#\"" );
+      assertThat( generatedJsonLd ).contains(
+            "\"samm\" : \"urn:samm:org.eclipse.esmf.samm:meta-model:" + KnownVersion.getLatest().toVersionString() + "#\"" );
       assertThat( generatedJsonLd ).contains( "\"@vocab\" : \"urn:samm:org.eclipse.esmf.test:1.0.0#\"" );
       assertThat( generatedJsonLd ).contains( String.format( "\"@id\" : \"%s\"", aspectModelUrn.toString() ) );
    }
