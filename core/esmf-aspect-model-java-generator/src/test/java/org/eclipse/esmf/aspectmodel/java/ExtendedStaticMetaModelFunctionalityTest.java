0706ccccad2df1c65602709924bce5910bd6f5a6
Yauhenikapl <external.Yauheni.Kapliarchuk@de.bosch.com>
2025-06-27 12:21:09 +0300

    Fix Optional.of for samm:optional true; property

--- core/esmf-aspect-model-java-generator/src/test/java/org/eclipse/esmf/aspectmodel/java/ExtendedStaticMetaModelFunctionalityTest.java
+++ core/esmf-aspect-model-java-generator/src/test/java/org/eclipse/esmf/aspectmodel/java/ExtendedStaticMetaModelFunctionalityTest.java
@@ -31,7 +31,7 @@ import org.eclipse.esmf.test.TestAspect;
 import org.apache.commons.lang3.reflect.ConstructorUtils;
 import org.junit.jupiter.api.Test;
 
-public class ExtendedStaticMetaModelFunctionalityTest extends StaticMetaModelGeneratorTest {
+class ExtendedStaticMetaModelFunctionalityTest extends StaticMetaModelGeneratorTest {
    @Test
    void testComputedProperties() throws IOException, ReflectiveOperationException {
       final TestAspect aspect = TestAspect.ASPECT_WITH_EXTENDED_ENUMS;
