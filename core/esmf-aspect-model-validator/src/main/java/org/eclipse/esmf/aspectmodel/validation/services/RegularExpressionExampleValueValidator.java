c95e8a257b6858a696dd30ade1765f684fb452f6
Textor Andreas (BCI/ESW17) <Andreas.Textor@de.bosch.com>
2025-12-16 13:52:10 +0100

    Simplify Regex validator and use XPath logic instead of RgxGen

--- core/esmf-aspect-model-validator/src/main/java/org/eclipse/esmf/aspectmodel/validation/services/RegularExpressionExampleValueValidator.java
+++ core/esmf-aspect-model-validator/src/main/java/org/eclipse/esmf/aspectmodel/validation/services/RegularExpressionExampleValueValidator.java
@@ -13,131 +13,46 @@
 
 package org.eclipse.esmf.aspectmodel.validation.services;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 import org.eclipse.esmf.aspectmodel.shacl.violation.Violation;
 import org.eclipse.esmf.aspectmodel.validation.RdfBasedValidator;
 import org.eclipse.esmf.aspectmodel.validation.RegularExpressionConstraintViolation;
-import org.eclipse.esmf.metamodel.vocabulary.SAMM;
-import org.eclipse.esmf.metamodel.vocabulary.SAMMC;
 import org.eclipse.esmf.metamodel.vocabulary.SammNs;
 
-import com.github.curiousoddman.rgxgen.RgxGen;
-import com.github.curiousoddman.rgxgen.parsing.dflt.RgxGenParseException;
+import com.google.common.collect.Streams;
+import org.apache.jena.rdf.model.Literal;
 import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.RDFNode;
-import org.apache.jena.rdf.model.Resource;
-import org.apache.jena.rdf.model.ResourceFactory;
 import org.apache.jena.rdf.model.Statement;
-import org.apache.jena.rdf.model.StmtIterator;
 import org.apache.jena.vocabulary.RDF;
+import org.apache.xerces.impl.xpath.regex.ParseException;
+import org.apache.xerces.impl.xpath.regex.RegularExpression;
 
+/**
+ * Validates that the samm:value of each samm-c:RegularExpressionConstraint adheres to
+ * <a href="https://www.w3.org/TR/xpath-functions-3/#regex-syntax">XQuery 1.0 and XPath 2.0 Functions and Operators</a>
+ */
 public class RegularExpressionExampleValueValidator implements RdfBasedValidator<Violation, List<Violation>> {
-
-   private final SAMM samm = SammNs.SAMM;
-   private final SAMMC sammc = SammNs.SAMMC;
-
-   private final List<Violation> violations = new ArrayList<>();
-
    @Override
    public List<Violation> validateModel( final Model model ) {
-      violations.clear();
-
-      // Traverse all Aspects in the model
-      final StmtIterator aspects = model.listStatements( null, RDF.type, samm.Aspect() );
-      while ( aspects.hasNext() ) {
-         final Statement aspect = aspects.nextStatement();
-         final Statement properties = aspect.getSubject().getProperty( samm.properties() );
-         if ( properties != null ) {
-            final Iterator<RDFNode> aspectProperties = properties.getList().iterator();
-            while ( aspectProperties.hasNext() ) {
-               final RDFNode propRef = aspectProperties.next();
-               traverseRecursively( propRef.asResource() );
-            }
-         }
-      }
-
-      return violations;
-   }
-
-   /**
-    * Recursively traverses the model starting from the given resource.
-    * Checks for RegularExpressionConstraint without exampleValue and descends into all relevant paths:
-    * - characteristic
-    * - dataType
-    * - baseCharacteristic
-    * - properties
-    */
-   private void traverseRecursively( final Resource resource ) {
-      checkRegularExpressionConstraint( resource );
-
-      // Traverse characteristic if present
-      Statement characteristicStmt = resource.getProperty( samm.characteristic() );
-      if ( characteristicStmt != null ) {
-         final Resource characteristic = characteristicStmt.getObject().asResource();
-         traverseRecursively( characteristic );
-
-         // Traverse dataType if present in characteristic
-         Statement dataTypeStmt = characteristic.getProperty( samm.dataType() );
-         if ( dataTypeStmt != null ) {
-            final Resource dataType = dataTypeStmt.getObject().asResource();
-            traverseRecursively( dataType );
-         }
-
-         // Traverse baseCharacteristic if present in characteristic
-         Statement baseCharacteristicStmt = characteristic.getProperty( sammc.baseCharacteristic() );
-         if ( baseCharacteristicStmt != null ) {
-            final Resource baseCharacteristic = baseCharacteristicStmt.getObject().asResource();
-            traverseRecursively( baseCharacteristic );
-         }
-      }
-
-      // Traverse properties if present
-      Statement propertiesStmt = resource.getProperty( samm.properties() );
-      if ( propertiesStmt != null ) {
-         Iterator<RDFNode> propertyNodes = propertiesStmt.getList().iterator();
-         while ( propertyNodes.hasNext() ) {
-            RDFNode node = propertyNodes.next();
-            if ( node.isResource() ) {
-               traverseRecursively( node.asResource() );
-            }
-         }
-      }
-   }
-
-   /**
-    * Checks if the resource has a RegularExpressionConstraint that we can't generate and no exampleValue exists.
-    * If so, adds a violation.
-    */
-   private void checkRegularExpressionConstraint( final Resource resource ) {
-      if ( resource.getProperty( samm.exampleValue() ) == null ) {
-         // Check for presence of constraint
-         final Statement characteristicStmt = resource.getProperty( samm.characteristic() );
-         if ( characteristicStmt != null ) {
-            Resource characteristic = characteristicStmt.getObject().asResource();
-            final Statement constraintStmt = characteristic.getProperty( sammc.constraint() );
-            if ( constraintStmt != null ) {
-               final Resource constraintResource = constraintStmt.getObject().asResource();
-               final Property typeProp = ResourceFactory.createProperty( "http://www.w3.org/1999/02/22-rdf-syntax-ns#type" );
-               boolean isRegExConstraint = constraintResource.hasProperty( typeProp, sammc.RegularExpressionConstraint() );
-               if ( isRegExConstraint ) {
-                  final Property valueProp = samm.value();
-                  String regexValue = constraintResource.getProperty( valueProp ).getString();
-                  try {
-                     RgxGen.parse( regexValue );
-                  } catch ( final RgxGenParseException exception ) {
-                     reportViolation( constraintResource, regexValue );
-                  }
-               }
-            }
-         }
-      }
-   }
-
-   private void reportViolation( final Resource constraintResource, final String regexValue ) {
-      violations.add( new RegularExpressionConstraintViolation( constraintResource, regexValue ) );
+      return Streams.stream( model.listStatements( null, RDF.type, SammNs.SAMMC.RegularExpressionConstraint() ) )
+            .filter( Objects::nonNull )
+            .map( Statement::getSubject )
+            .flatMap( constraint -> Streams.stream( model.listStatements( constraint, SammNs.SAMM.value(), (RDFNode) null ) )
+                  .filter( Objects::nonNull )
+                  .map( Statement::getLiteral )
+                  .map( Literal::getString )
+                  .<Violation> flatMap( regex -> {
+                     try {
+                        new RegularExpression( regex, "X" );
+                        return Stream.of();
+                     } catch ( final ParseException exception ) {
+                        return Stream.of( new RegularExpressionConstraintViolation( constraint, regex ) );
+                     }
+                  } ) )
+            .toList();
    }
 }
