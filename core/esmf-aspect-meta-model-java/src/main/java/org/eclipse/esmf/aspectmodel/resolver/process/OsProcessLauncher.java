a2e0bfc5c6c619128c4505433c7b1db16e88d5f9
efilchenko <fvi2fe@bosch.com>
2025-12-29 14:21:49 +0300

    add debug

--- core/esmf-aspect-meta-model-java/src/main/java/org/eclipse/esmf/aspectmodel/resolver/process/OsProcessLauncher.java
+++ core/esmf-aspect-meta-model-java/src/main/java/org/eclipse/esmf/aspectmodel/resolver/process/OsProcessLauncher.java
@@ -60,8 +60,10 @@ public class OsProcessLauncher extends ProcessLauncher<Process> {
                      .skip( 1 )
                      .map( argument -> String.format( "\"%s\"", argument ) )
                      .collect( Collectors.joining( " " ) ) );
+         final File dir = workingDirectoryForSubprocess( context );
+         System.out.println( "Working directory: " + dir.getAbsolutePath() );
          final Process process = Runtime.getRuntime()
-               .exec( commandWithAllArguments.toArray( new String[0] ), null, workingDirectoryForSubprocess( context ) );
+               .exec( commandWithAllArguments.toArray( new String[0] ), null, dir );
          if ( context.stdin().isPresent() ) {
             IOUtils.copy( new ByteArrayInputStream( context.stdin().get() ), process.getOutputStream() );
          }
