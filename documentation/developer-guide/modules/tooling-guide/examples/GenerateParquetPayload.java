e3e126da1ff18710c10a5930b50ae681c64d8300
RHJ5FE <shijin.raj@de.bosch.com>
2026-02-25 16:44:37 +0100

    feat: add GenerateParquetPayload class for Apache Parquet payload generation

---
+++ documentation/developer-guide/modules/tooling-guide/examples/GenerateParquetPayload.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2023 Robert Bosch Manufacturing Solutions GmbH
+ *
+ * See the AUTHORS file(s) distributed with this work for additional
+ * information regarding authorship.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+package examples;
+
+// tag::imports[]
+
+import java.io.File;
+import java.io.IOException;
+
+import org.eclipse.esmf.aspectmodel.generator.parquet.AspectModelParquetPayloadGenerator;
+import org.eclipse.esmf.aspectmodel.loader.AspectModelLoader;
+import org.eclipse.esmf.metamodel.AspectModel;
+import org.junit.jupiter.api.Test;
+
+public class GenerateParquetPayload extends AbstractGenerator {
+   @Test
+   public void generate() throws IOException {
+
+      // AspectModel as returned by the AspectModelLoader
+      final AspectModel aspectModel = // ...
+
+            new AspectModelLoader().load(
+                  new File( "aspect-models/org.eclipse.esmf.examples.movement/1.0.0/Movement.ttl" ) );
+
+      final AspectModelParquetPayloadGenerator generator = new AspectModelParquetPayloadGenerator( aspectModel.aspect() );
+
+      // Direct generation of Apache Parquet payload
+      generator.generate().findFirst().orElseThrow( () -> new IOException( "Failed to generate parquet artifact" ) );
+
+      // end
+   }
+}
