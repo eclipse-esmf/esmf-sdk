5b8cf45914d0912dccbeaa9b2b84a0bdde2d3931
Andreas Textor <Andreas.Textor@de.bosch.com>
2026-03-05 13:20:18 +0100

    Fix Linux and MacOS launcher scripts

--- tools/samm-cli/scripts/macos/run.sh
+++ tools/samm-cli/scripts/macos/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-HERE="\${BASH_SOURCE%/*}"
-"\$HERE/jre/Contents/Home/bin/java" --enable-native-access=ALL-UNNAMED --sun-misc-unsafe-memory-access=allow -Dpolyglotimpl.DisableMultiReleaseCheck=true -jar "\$HERE/samm-cli-*.jar" "\$@"
+DIR="${BASH_SOURCE%/*}"
+exec "$DIR/jre/Contents/Home/bin/java" --enable-native-access=ALL-UNNAMED --sun-misc-unsafe-memory-access=allow -Dpolyglotimpl.DisableMultiReleaseCheck=true -jar "$DIR"/samm-cli-*.jar "$@"
