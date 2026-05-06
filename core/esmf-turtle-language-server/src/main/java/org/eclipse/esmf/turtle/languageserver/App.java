5dae4579eaf7706e535ed6f639882289ce8db7a9
Andreas Textor <Andreas.Textor@de.bosch.com>
2026-05-06 15:24:20 +0200

    Start LSP server on local port

--- core/esmf-turtle-language-server/src/main/java/org/eclipse/esmf/turtle/languageserver/App.java
+++ core/esmf-turtle-language-server/src/main/java/org/eclipse/esmf/turtle/languageserver/App.java
@@ -13,34 +13,42 @@
 
 package org.eclipse.esmf.turtle.languageserver;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.Channels;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.function.Function;
+
 import org.eclipse.lsp4j.jsonrpc.Launcher;
 import org.eclipse.lsp4j.services.LanguageClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class App {
+   public static final int PORT = 1846;
    private static final Logger LOG = LoggerFactory.getLogger( App.class );
 
    static void main( final String[] args ) {
-      LOG.info( "Starting lsp-server" );
-      final TurtleLanguageServer server = new TurtleLanguageServer();
-
-      try {
-         final Launcher<LanguageClient> launcher = Launcher.createLauncher(
-               server,
-               LanguageClient.class,
-               System.in,
-               System.out
-         );
-
-         server.connect( launcher.getRemoteProxy() );
-         launcher.startListening().get();
-         LOG.info( "Language server listener stopped" );
-      } catch ( final InterruptedException ex ) {
-         Thread.currentThread().interrupt();
-         LOG.error( "Language server listener was interrupted", ex );
-      } catch ( final Exception ex ) {
-         LOG.error( "Language server terminated with an error", ex );
+      LOG.info( "Starting lsp-server on port {}", PORT );
+      final TurtleLanguageServer languageServer = new TurtleLanguageServer();
+      try ( final AsynchronousServerSocketChannel serverSocket = AsynchronousServerSocketChannel.open() ) {
+         serverSocket.bind( new InetSocketAddress( "localhost", PORT ) );
+         final AsynchronousSocketChannel socketChannel = serverSocket.accept().get();
+         final Launcher<LanguageClient> launcher =
+               Launcher.createIoLauncher( languageServer, LanguageClient.class, Channels.newInputStream( socketChannel ),
+                     Channels.newOutputStream( socketChannel ), Executors.newCachedThreadPool(), Function.identity() );
+         final Future<?> future = launcher.startListening();
+         languageServer.connect( launcher.getRemoteProxy() );
+         while ( !future.isDone() ) {
+            // noinspection BusyWait
+            Thread.sleep( 10_000L );
+         }
+      } catch ( final InterruptedException | ExecutionException | IOException exception ) {
+         LOG.warn( "Could not launch Language Server", exception );
       }
    }
 }
